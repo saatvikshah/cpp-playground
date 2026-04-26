5dbc5e4ab1c9cd651d457c935bd2f84b953ef3c5
Saatvik Shah <saatvikshah1994@gmail.com>
2026-04-26 14:17:03 +0530

    Phase 3: Add custom deleter functionality and few other TODOs

--- projects/2_my_uniq_ptr/my_uniq_ptr.hpp
+++ projects/2_my_uniq_ptr/my_uniq_ptr.hpp
@@ -4,20 +4,35 @@
 
 /**
  * TODO:
- * - Custom deleter
- * - release/reset/swap
  * - make_unique
  * - Array specialization
- * - operator*, operator->, operator bool
- * - get() const correctness
+ * - operator*, operator->
  * - Converting move support
  * - Benchmark
+ *
+ * Lessons:
+ * - `delete <x>` does not need a check if it is nullptr or not
+ * - Templated deleter type: functor providing an operator()(T*), useful for file closing as an example.
+ * - Testing: type trait checks for compile-time behavior, destruction checked with stubs
  */
 namespace code {
-template <typename T>
+template <typename T, typename Deleter = std::default_delete<T>>
 struct unique_ptr {
 
     explicit unique_ptr(T* ptr): ptr_(ptr) {}
+    explicit unique_ptr(T* ptr, Deleter deleter): ptr_(ptr), deleter_(deleter) {}
+
+    T* release() {
+        return std::exchange(ptr_ , nullptr);
+    }
+
+    void reset(T* ptr) {
+        if (ptr_ == ptr) {
+            return;
+        }
+        T* old = std::exchange(ptr_, ptr);
+        deleter_(old);
+    }
 
     T* get() {
         return ptr_;
@@ -28,14 +43,30 @@ struct unique_ptr {
         std::swap(ptr_, other.ptr_);
     }
 
+    operator bool() const noexcept
+    {
+        return ptr_ != nullptr;
+    }
+
+    T& operator *()
+    {
+        return *ptr_;
+    }
+
+    T* operator ->()
+    {
+        return ptr_;
+    }
+
     unique_ptr& operator=(unique_ptr&& other) noexcept
     {
         if (this == &other) {
             return *this;
         }
-        delete ptr_;
-        ptr_ = nullptr;
-        std::swap(ptr_, other.ptr_);
+        T* old = std::exchange(ptr_, other.ptr_);
+        deleter_(old);
+        other.ptr_ = nullptr;
+        std::swap(deleter_, other.deleter_);
         return *this;
     }
 
@@ -43,10 +74,11 @@ struct unique_ptr {
     unique_ptr& operator=(const unique_ptr& other) = delete;
 
     ~unique_ptr() {
-        delete ptr_;
+        deleter_(ptr_);
     }
 
 private:
     T* ptr_{nullptr};
+    Deleter deleter_;
 };
 }  // namespace my_uniq_ptr
--- projects/2_my_uniq_ptr/tests/my_uniq_ptr_test.cpp
+++ projects/2_my_uniq_ptr/tests/my_uniq_ptr_test.cpp
@@ -18,10 +18,24 @@ struct TestStub {
   }
 };
 
+struct TestDeleter {
+  bool& called_;
+  explicit TestDeleter(bool& called): called_(called) {
+    called_ = false;
+  }
+
+  void operator()(int* ptr) const {
+    called_ = true;
+    delete ptr;
+  }
+
+};
+
 TEST(my_uniq_ptr, HoldsUnderlyingPointer) {
   auto* elem = new int(10);
   {
     code::unique_ptr elem_ptr{elem};
+    EXPECT_TRUE(elem_ptr);
     EXPECT_THAT(elem_ptr.get(), Eq(elem));
   }
 }
@@ -34,6 +48,16 @@ TEST(my_uniq_ptr, DestroyedWhenOutOfScope) {
     code::unique_ptr elem_ptr{elem};
   }
   EXPECT_THAT(check_destroyed, true);
+}
+
+TEST(my_uniq_ptr, DestroyedByCustomDeleter) {
+  bool check_destroyed;
+  auto* elem = new int(3);
+  {
+    code::unique_ptr elem_ptr{elem, TestDeleter{check_destroyed}};
+    EXPECT_FALSE(check_destroyed);
+  }
+  EXPECT_TRUE(check_destroyed);
 }
 
 
@@ -85,4 +109,22 @@ TEST(my_uniq_ptr, MoveAssignmentPreventsSelfMove) {
     EXPECT_EQ(elem_ptr1.get(), elem1);
   }
   EXPECT_TRUE(e1destroyed);
+}
+
+TEST(my_uniq_ptr, ResetSwitchesOwnership_DestroysOld) {
+  bool e1destroyed;
+  bool e2destroyed;
+  auto* elem1 = new TestStub(e1destroyed);
+  auto* elem2 = new TestStub(e2destroyed);
+  {
+    EXPECT_FALSE(e1destroyed);
+    EXPECT_FALSE(e2destroyed);
+    code::unique_ptr elem_ptr{elem1};
+    EXPECT_EQ(elem_ptr.get(), elem1);
+    elem_ptr.reset(elem2);
+    EXPECT_TRUE(e1destroyed);
+    EXPECT_FALSE(e2destroyed);
+    EXPECT_EQ(elem_ptr.get(), elem2);
+  }
+  EXPECT_TRUE(e2destroyed);
 }
